1acda121148fb7cbe1cc71d4663e3ee66edfbfbd
devalmazbek <devalmazbek@gmail.com>
2025-02-15 16:23:29 +0600

    implementation: widget statistic, ui component card

---
+++ src/shared/ui/card/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import Icon from '../../icon';
+
+interface CardProps {
+  title: string;
+  subtitle?: string;
+  icon?: IconDefinition;
+  withIcon?: boolean; 
+  description: string;
+  className?: string;
+}
+
+export default function Card({ title, subtitle, icon, withIcon = false, description, className }: CardProps) {
+  return (
+    <div className={`min-w-48 p-3 rounded-md shadow shadow-slate-400 ${className}`}>
+      {withIcon && icon && <Icon icon={icon} />}
+      <div className='mt-3'>
+        <p className='mb-3'>
+          <span className='text-4xl font-semibold text-emerald-600 uppercase'>{title}</span>
+          {subtitle && <span> - {subtitle}</span>}
+        </p>
+        <p className='uppercase text-sm'>{description}</p>
+      </div>
+    </div>
+  );
+}
